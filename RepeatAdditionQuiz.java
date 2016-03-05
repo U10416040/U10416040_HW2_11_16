b3e3059f7f6baa424a800c07f1cd148d6045a75f
U10416040 <u10416040@go.utaipei.edu.tw>
2016-03-05 17:13:44 +0800

    add comments
    important line had been add comments

--- RepeatAdditionQuiz.java
+++ RepeatAdditionQuiz.java
@@ -1,13 +1,15 @@
+//U10416040 ªL¦ö¼á
+
 import java.util.Scanner;
 import java.util.*;
 import java.awt.*;
 
 
 public class RepeatAdditionQuiz {
-	static ArrayList<Integer> store;
+	static ArrayList<Integer> store;//delcare static variable
 
 	public static void main(String[] args){
-		store=new ArrayList<>();
+		store=new ArrayList<>();//create ArraryList object
 		
 		int number1=(int)(Math.random()*10);
 		int number2=(int)(Math.random()*10);
@@ -16,16 +18,17 @@ public class RepeatAdditionQuiz {
 
 		System.out.print("What is " + number1 + "+" + number2 + "?");
 		int answer=input.nextInt();
-		store.add(answer);
+		store.add(answer);//append the answer to the ArraryList
 		
 		while(number1 + number2!=answer){
 			System.out.print("Wrong answer. Try again. What is " + number1 + "+" + number2 + "?");
 			answer=input.nextInt();
 
+			//judge if the answer is in the ArrayList
 			if(store.contains(answer))
 				System.out.println("You already entered " + answer);
 			else
-				store.add(answer);
+				store.add(answer);//append the answer to the ArraryList
 		}
 		System.out.println("You got it!");
 	}
