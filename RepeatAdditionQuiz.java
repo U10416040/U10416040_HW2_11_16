f19ef59c54837f1f7a973bca431d73edd46af641
U10416040 <u10416040@go.utaipei.edu.tw>
2016-03-05 17:08:47 +0800

    Add ArraryList
     Use ArraryList to store the answer input before.
    Use contain method to judge if the answer exists in the ArrayList.

--- RepeatAdditionQuiz.java
+++ RepeatAdditionQuiz.java
@@ -4,11 +4,8 @@ import java.awt.*;
 
 
 public class RepeatAdditionQuiz {
-	
-	
 	static ArrayList<Integer> store;
 
-	
 	public static void main(String[] args){
 		store=new ArrayList<>();
 		
@@ -16,15 +13,20 @@ public class RepeatAdditionQuiz {
 		int number2=(int)(Math.random()*10);
 		
 		Scanner input=new Scanner(System.in);
-		
+
 		System.out.print("What is " + number1 + "+" + number2 + "?");
 		int answer=input.nextInt();
+		store.add(answer);
 		
 		while(number1 + number2!=answer){
 			System.out.print("Wrong answer. Try again. What is " + number1 + "+" + number2 + "?");
 			answer=input.nextInt();
+
+			if(store.contains(answer))
+				System.out.println("You already entered " + answer);
+			else
+				store.add(answer);
 		}
-		
 		System.out.println("You got it!");
 	}
 }
